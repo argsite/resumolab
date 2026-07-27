c2e329921ba91211af9cdb96ab3f93493b319d56
Miro Teixeira <78441204+argsite@users.noreply.github.com>
2026-07-27 20:26:06 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -35,10 +35,12 @@ if uploaded_file is not None and api_key:
         """
         
         # 4. Chamada ao modelo Gemini
+                # Chamada ao modelo Gemini corrigida
         response = client.models.generate_content(
             model='gemini-2.5-flash',
             contents=prompt,
         )
+
         
         resumo_limpo = response.text
         
